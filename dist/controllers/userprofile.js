a900d4577fde44b630fcb444aaebcdf1b04f120c
dvgiang <duongvangianghhbg152@gmail.com>
2024-05-25 23:22:07 +0700

    phase5

--- dist/controllers/userprofile.js
+++ dist/controllers/userprofile.js
@@ -5,31 +5,36 @@ const express_1 = require("express");
 exports.customerProfileRouter = (0, express_1.Router)();
 const mockData = [
     {
-        id: "1231231",
+        id: "1231232",
         username: "Duong van giang",
         phonenumber: "092137128371928"
     },
     {
-        id: "1231231",
+        id: "1231233",
         username: "Duong van giang",
         phonenumber: "092137128371928"
     },
     {
-        id: "1231231",
+        id: "1231234",
         username: "Duong van giang",
         phonenumber: "092137128371928"
     },
     {
-        id: "1231231",
+        id: "1231235",
         username: "Duong van giang",
         phonenumber: "092137128371928"
     },
     {
-        id: "1231231",
+        id: "1231236",
         username: "Duong van giang",
         phonenumber: "092137128371928"
     },
 ];
+const mockData2 = {
+    id: "3213123",
+    username: "Dương Văn Giang",
+    phonenumber: "Phone number"
+};
 exports.customerProfileRouter.get("/search", async (req, res) => {
     try {
         console.log(req.query);
@@ -41,6 +46,8 @@ exports.customerProfileRouter.get("/search", async (req, res) => {
 });
 exports.customerProfileRouter.get("/:id", async (req, res) => {
     try {
+        console.log(req.params);
+        return res.status(200).send(mockData2);
     }
     catch (err) {
         return res.status(500).send({ message: "Unknown err" });
--- src/controllers/userprofile.ts
+++ src/controllers/userprofile.ts
@@ -4,35 +4,42 @@ export const customerProfileRouter = Router();
 
 const mockData = [
     {
-        id: "1231231",
+        id: "1231232",
         username: "Duong van giang",
         phonenumber: "092137128371928"
     },
     {
-        id: "1231231",
+        id: "1231233",
         username: "Duong van giang",
         phonenumber: "092137128371928"
     },
     {
-        id: "1231231",
+        id: "1231234",
         username: "Duong van giang",
         phonenumber: "092137128371928"
     },
     {
-        id: "1231231",
+        id: "1231235",
         username: "Duong van giang",
         phonenumber: "092137128371928"
     },
     {
-        id: "1231231",
+        id: "1231236",
         username: "Duong van giang",
         phonenumber: "092137128371928"
     },
 ]
 
+const mockData2 = {
+    id: "3213123",
+    username: "Dương Văn Giang",
+    phonenumber: "Phone number"
+
+}
+
 customerProfileRouter.get("/search", async (req: Request, res: Response) => {
     try{
-        console.log(req.query);
+        console.log(req.query)
         
         return res.send(mockData);
     } catch(err) {
@@ -44,7 +51,8 @@ customerProfileRouter.get("/search", async (req: Request, res: Response) => {
 
 customerProfileRouter.get("/:id",async (req: Request, res: Response)  => {
     try{
-      
+      console.log(req.params);
+      return res.status(200).send(mockData2)
     } catch(err) {
         return res.status(500).send({message: "Unknown err"})
     }
